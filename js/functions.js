ec5fe7af11c7a2a81639292780b3a37cc6acac0a
webkey <serhiy_i@ukr.net>
2016-01-04 16:12:18 +0300

    1

--- js/functions.js
+++ js/functions.js
@@ -134,13 +134,43 @@ function showInput(){
 
 /*custom scroll init*/
 function customScrollInit(){
-	$(".panel-frame, .drop-visible__holder").mCustomScrollbar({
-		//axis:"x",
-		theme:"minimal-dark",
-		scrollbarPosition: "inside",
-		autoExpandScrollbar:true
-		//advanced:{autoExpandHorizontalScroll:true}
-	});
+	/*main navigation*/
+	if($('.panel-frame').length){
+		$('.panel-frame, .drop-visible__holder').mCustomScrollbar({
+			//axis:"x",
+			theme:"minimal-dark",
+			scrollbarPosition: "inside",
+			autoExpandScrollbar:true
+			//advanced:{autoExpandHorizontalScroll:true}
+		});
+	}
+	/*main navigation end*/
+
+	/*rubrics thumbs*/
+	var $rubricsThumbs = $(".rubrics-thumbs");
+	if($rubricsThumbs.length){
+		$rubricsThumbs.mCustomScrollbar({
+			axis:"x",
+			//theme:"dark",
+			scrollbarPosition: "inside",
+			//autoExpandScrollbar:true,
+			advanced:{autoExpandHorizontalScroll:true}
+		});
+	}
+	/*rubrics thumbs end*/
+
+	/*rubrics minimal*/
+	var $rubricsMinimal = $(".rubrics-minimal");
+	if($rubricsMinimal.length){
+		$rubricsMinimal.mCustomScrollbar({
+			axis:"x",
+			theme:"minimal-dark",
+			scrollbarPosition: "inside"
+			//autoExpandScrollbar:true,
+			//advanced:{autoExpandHorizontalScroll:true}
+		});
+	}
+	/*rubrics minimal end*/
 }
 /*custom scroll init end*/
 
@@ -258,24 +288,243 @@ function slickSlidersInit(){
 			//autoplaySpeed: 3000,
 			dots: true,
 			cssEase: 'ease-in-out',
-			arrow: false
+			arrows: false
 		});
 	}
 	/*promo slider end*/
+
+	/*promo slider*/
+	var sliderRubricsFull = $('.rubrics-full');
+	if(sliderRubricsFull.length){
+		sliderRubricsFull.slick({
+			//fade: true,
+			speed: 300,
+			infinite: true,
+			//autoplay: true,
+			//autoplaySpeed: 3000,
+			dots: false,
+			//cssEase: 'ease-in-out',
+			arrows: false
+		});
+	}
+	/*promo slider end*/
+
+	/*departments slider*/
+	var sliderDepartments = $('.departments-slider');
+	if(sliderDepartments.length){
+		sliderDepartments.slick({
+			slidesToShow: 4,
+			slidesToScroll: 4,
+			speed: 300,
+			infinite: false,
+			dots: true
+		});
+	}
+	/*departments slider end*/
 }
 /*slick sliders init end*/
 
-/*products slider*/
-function productsSliderInit(){
-	$(".products-slider__thumbs-list").mCustomScrollbar({
-		axis:"x",
-		theme:"minimal-dark",
-		scrollbarPosition: "inside",
-		autoExpandScrollbar:true
-		//advanced:{autoExpandHorizontalScroll:true}
-	});
+/*map init*/
+function mapInit(){
+	google.maps.event.addDomListener(window, 'load', init);
+	var map;
+	function init() {
+		var mapOptions = {
+			center: new google.maps.LatLng(53.57782335899915,22.72702740340762),
+			zoom: 6,
+			zoomControl: true,
+			zoomControlOptions: {
+				style: google.maps.ZoomControlStyle.DEFAULT,
+			},
+			disableDoubleClickZoom: true,
+			mapTypeControl: false,
+			scaleControl: false,
+			scrollwheel: false,
+			panControl: true,
+			streetViewControl: false,
+			draggable : true,
+			overviewMapControl: true,
+			overviewMapControlOptions: {
+				opened: false,
+			},
+			mapTypeId: google.maps.MapTypeId.ROADMAP,
+			styles: [
+				{
+					"featureType": "water",
+					"elementType": "geometry.fill",
+					"stylers": [
+						{ "color": "#d3d3d3" }
+					]
+				},{
+					"featureType": "transit",
+					"stylers": [
+						{ "color": "#808080" },
+						{ "visibility": "off" }
+					]
+				},{
+					"featureType": "road.highway",
+					"elementType": "geometry.stroke",
+					"stylers": [
+						{ "visibility": "on" },
+						{ "color": "#b3b3b3" }
+					]
+				},{
+					"featureType": "road.highway",
+					"elementType": "geometry.fill",
+					"stylers": [
+						{ "color": "#ffffff" }
+					]
+				},{
+					"featureType": "road.local",
+					"elementType": "geometry.fill",
+					"stylers": [
+						{ "visibility": "on" },
+						{ "color": "#ffffff" },
+						{ "weight": 1.8 }
+					]
+				},{
+					"featureType": "road.local",
+					"elementType": "geometry.stroke",
+					"stylers": [
+						{ "color": "#d7d7d7" }
+					]
+				},{
+					"featureType": "poi",
+					"elementType": "geometry.fill",
+					"stylers": [
+						{ "visibility": "on" },
+						{ "color": "#ebebeb" }
+					]
+				},{
+					"featureType": "administrative",
+					"elementType": "geometry",
+					"stylers": [
+						{ "color": "#a7a7a7" }
+					]
+				},{
+					"featureType": "road.arterial",
+					"elementType": "geometry.fill",
+					"stylers": [
+						{ "color": "#ffffff" }
+					]
+				},{
+					"featureType": "road.arterial",
+					"elementType": "geometry.fill",
+					"stylers": [
+						{ "color": "#ffffff" }
+					]
+				},{
+					"featureType": "landscape",
+					"elementType": "geometry.fill",
+					"stylers": [
+						{ "visibility": "on" },
+						{ "color": "#efefef" }
+					]
+				},{
+					"featureType": "road",
+					"elementType": "labels.text.fill",
+					"stylers": [
+						{ "color": "#696969" }
+					]
+				},{
+					"featureType": "administrative",
+					"elementType": "labels.text.fill",
+					"stylers": [
+						{ "visibility": "on" },
+						{ "color": "#737373" }
+					]
+				},{
+					"featureType": "poi",
+					"elementType": "labels.icon",
+					"stylers": [
+						{ "visibility": "off" }
+					]
+				},{
+					"featureType": "poi",
+					"elementType": "labels",
+					"stylers": [
+						{ "visibility": "off" }
+					]
+				},{
+					"featureType": "road.arterial",
+					"elementType": "geometry.stroke",
+					"stylers": [
+						{ "color": "#d6d6d6" }
+					]
+				},{
+					"featureType": "road",
+					"elementType": "labels.icon",
+					"stylers": [
+						{ "visibility": "off" }
+					]
+				},{
+				},{
+					"featureType": "poi",
+					"elementType": "geometry.fill",
+					"stylers": [
+						{ "color": "#dadada" }
+					]
+				}
+			],
+		}
+		var mapElement = document.getElementById('map-niva-holding');
+		var map = new google.maps.Map(mapElement, mapOptions);
+		var locations = [
+			['ул. Заводская, 4', 'Республика Беларусь, Минская область, Солигорский район', 'undefined', 'undefined', 'undefined', 52.66995207146201, 27.48641749999999, 'img/map-niva-pin.png']
+		];
+		for (i = 0; i < locations.length; i++) {
+			if (locations[i][1] =='undefined'){ description ='';} else { description = locations[i][1];}
+			if (locations[i][2] =='undefined'){ telephone ='';} else { telephone = locations[i][2];}
+			if (locations[i][3] =='undefined'){ email ='';} else { email = locations[i][3];}
+			if (locations[i][4] =='undefined'){ web ='';} else { web = locations[i][4];}
+			if (locations[i][7] =='undefined'){ markericon ='';} else { markericon = locations[i][7];}
+			marker = new google.maps.Marker({
+				icon: markericon,
+				position: new google.maps.LatLng(locations[i][5], locations[i][6]),
+				map: map,
+				title: locations[i][0],
+				desc: description,
+				tel: telephone,
+				email: email,
+				web: web
+			});
+			link = '';            bindInfoWindow(marker, map, locations[i][0], description, telephone, email, web, link);
+		}
+		function bindInfoWindow(marker, map, title, desc, telephone, email, web, link) {
+			var infoWindowVisible = (function () {
+				var currentlyVisible = false;
+				return function (visible) {
+					if (visible !== undefined) {
+						currentlyVisible = visible;
+					}
+					return currentlyVisible;
+				};
+			}());
+			iw = new google.maps.InfoWindow();
+			google.maps.event.addListener(marker, 'click', function() {
+				if (infoWindowVisible()) {
+					iw.close();
+					infoWindowVisible(false);
+				} else {
+					var html= "<div style='color:#000;background-color:#fff;padding:5px;width:150px;'><h4>"+title+"</h4><p>"+desc+"<p></div>";
+					iw = new google.maps.InfoWindow({content:html});
+					iw.open(map,marker);
+					infoWindowVisible(true);
+				}
+			});
+			google.maps.event.addListener(iw, 'closeclick', function () {
+				infoWindowVisible(false);
+			});
+		}
+	}
 }
-/*products slider end*/
+/*map init end*/
+
+/*rubrics slider*/
+function rubricsSliderInit(){
+
+}
+/*rubrics slider end*/
 
 /** ready/load/resize document **/
 
@@ -288,7 +537,8 @@ $(document).ready(function(){
 	//breadDrop();
 	breadHover();
 	slickSlidersInit();
-	productsSliderInit();
+	rubricsSliderInit();
+	mapInit();
 });
 $(window).load(function () {
 	customScrollInit();
