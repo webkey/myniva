540c2e98ce24c96b211810331bd88becf00c4bc8
Serhii <serhiy.ilchenko@gmail.com>
2016-01-13 03:39:57 +0300

    home 28

--- js/functions.js
+++ js/functions.js
@@ -1028,6 +1028,80 @@ function masonryInit(){
 }
 /*masonry initial end*/
 
+/*scroll navigation*/
+$.extend($.easing, {
+		def: 'easeOutQuad', easeInOutExpo: function (x, t, b, c, d) {
+			if (t == 0) return b;
+			if (t == d) return b + c;
+			if ((t /= d / 2) < 1) return c / 2 * Math.pow(2, 10 * (t - 1)) + b;
+			return c / 2 * (-Math.pow(2, -10 * --t) + 2) + b;
+		}
+	});
+
+(function( $ ) {
+
+	var settings;
+	var disableScrollFn = false;
+	var navItems;
+	var navs = {}, sections = {};
+
+	$.fn.navScroller = function(options) {
+		settings = $.extend({
+			scrollToOffset: 0,
+			scrollSpeed: 800,
+			activateParentNode: true,
+		}, options );
+		navItems = this;
+
+		//attatch click listeners
+		navItems.on('click', function(event){
+			event.preventDefault();
+			var navID = $(this).attr("href").substring(1);
+			disableScrollFn = true;
+			activateNav(navID);
+			populateDestinations(); //recalculate these!
+			$('html,body').animate({scrollTop: sections[navID] - settings.scrollToOffset},
+				settings.scrollSpeed, "easeInOutExpo", function(){
+					disableScrollFn = false;
+				}
+			);
+		});
+
+		//populate lookup of clicable elements and destination sections
+		populateDestinations(); //should also be run on browser resize, btw
+
+		// setup scroll listener
+		$(document).scroll(function(){
+			if (disableScrollFn) { return; }
+			var page_height = $(window).height();
+			var pos = $(this).scrollTop();
+			for (i in sections) {
+				if ((pos + settings.scrollToOffset >= sections[i]) && sections[i] < pos + page_height){
+					activateNav(i);
+				}
+			}
+		});
+	};
+
+	function populateDestinations() {
+		navItems.each(function(){
+			var scrollID = $(this).attr('href').substring(1);
+			navs[scrollID] = (settings.activateParentNode)? this.parentNode : this;
+			sections[scrollID] = $(document.getElementById(scrollID)).offset().top;
+		});
+	}
+
+	function activateNav(navID) {
+		for (nav in navs) { $(navs[nav]).removeClass('active'); }
+		$(navs[navID]).addClass('active');
+	}
+})( jQuery );
+
+function scrollNavInit(){
+	$('.agents-previews__item a').navScroller();
+}
+/*scroll navigation end*/
+
 /** ready/load/resize document **/
 
 $(document).ready(function(){
@@ -1044,12 +1118,13 @@ $(document).ready(function(){
 	mapInitContacts();
 	fancyboxInit();
 	productGalleryInit();
-	accordionInit();
-	tabsInit();
 	openGallery();
+	scrollNavInit();
 });
 $(window).load(function () {
 	owlInit();
 	customScrollInit();
 	masonryInit();
+	accordionInit();
+	tabsInit();
 });
